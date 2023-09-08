852ea82a793e1c3246b0871a3be50189006aa225
daveonthegit <114205178+daveonthegit@users.noreply.github.com>
2023-09-08 09:33:26 -0400

    Update ArrayBag.cpp

--- ArrayBag.cpp
+++ ArrayBag.cpp
@@ -25,3 +25,30 @@ bool ArrayBag<T>::add(const T& new_entry){
     }
     return false;
 }
+template<class T>
+bool ArrayBag<T>::remove(const T& an_entry) {
+    int temp_index = getIndexOf(an_entry);
+    if(temp_index>=0){
+        item_count_--;
+        items_[temp_index]=items_[item_count_];
+        return true;
+    }
+    return false;
+}
+template<class T>
+bool ArrayBag<T>::contains(const T& an_entry) const{
+    if(getIndexOf(an_entry)>=0){
+        return true;
+    }
+    return false;
+}
+
+template<class T>
+int ArrayBag<T>::getIndexOf(const T& target) const{
+    for(int i = 0; i<item_count_;i++){
+        if(items_[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
